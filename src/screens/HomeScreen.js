bcef1d08a1bbbed1e892f3dd018cba3888980562
Lelis19 <tiago.lelis47@gmail.com>
2025-05-08 14:11:21 -0300

    Aula3

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useCallback, useState } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { getCursos } from '../services/CursoService'
+
+const HomeScreen = ({ navigation }) => {
+
+  const [items, setItems] = useState([])
+
+  const carregarCursos =  async () => {
+    const cursos = await getCursos()
+    setItems(cursos)
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      carregarCursos()
+    }, [])
+  )
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>📚 Cursos Disponíveis</Text>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+            <TouchableOpacity
+             style={styles.itemContainer}
+             onPress={() => navigation.navigate('Details', {
+              itemId: item.id,
+              name: item.name,
+              description: item.description
+             })}
+            >
+                <Text style={styles.itemTitle}>{item.name}</Text>
+                <Text style={styles.itemDescription}>{item.description}</Text>
+            </TouchableOpacity>
+        )
+
+        }
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 20, backgroundColor: '#f5f5f5' },
+    title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20 },
+    itemContainer: { backgroundColor: '#fff', padding: 15, marginBottom: 10, borderRadius: 8, elevation: 2 },
+    itemTitle: { fontSize: 18, fontWeight: 'bold' },
+    itemDescription: { fontSize: 14, color: '#555' },
+  })
+
+export default HomeScreen
